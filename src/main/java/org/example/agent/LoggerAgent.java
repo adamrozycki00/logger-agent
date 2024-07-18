8a4749952748ffe8b73c98f802759782305aa749
arozycki <arozycki@weborama.com>
2024-07-18 15:50:04 +0200

    Set up logger agent

---
+++ src/main/java/org/example/agent/LoggerAgent.java
@@ -0,0 +1,41 @@
+package org.example.agent;
+
+import static net.bytebuddy.matcher.ElementMatchers.any;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import org.example.agent.interceptor.Log4JInterceptor;
+import org.example.agent.interceptor.ToStringInterceptor;
+
+import java.lang.instrument.Instrumentation;
+
+public class LoggerAgent {
+
+  private static final String LOGGER_CLASS_TO_INTERCEPT = "org.apache.logging.log4j.core.Logger";
+  private static final String LOGGER_METHOD_TO_INTERCEPT = "logIfEnabled";
+
+  @SuppressWarnings("unused")
+  public static void premain(String arguments, Instrumentation instrumentation) {
+    loggerAgent().installOn(instrumentation);
+    toStringAgent().installOn(instrumentation);
+  }
+
+  private static AgentBuilder.Identified.Extendable toStringAgent() {
+    return new AgentBuilder.Default()
+        .type(any())
+        .transform((builder, _1, _2, _3, _4) ->
+            builder
+                .method(named("toString"))
+                .intercept(Advice.to(ToStringInterceptor.class)));
+  }
+
+  private static AgentBuilder.Identified.Extendable loggerAgent() {
+    return new AgentBuilder.Default()
+        .type(named(LOGGER_CLASS_TO_INTERCEPT))
+        .transform((builder, _1, _2, _3, _4) ->
+            builder
+                .method(named(LOGGER_METHOD_TO_INTERCEPT))
+                .intercept(Advice.to(Log4JInterceptor.class)));
+  }
+}
---
+++ src/main/java/org/example/agent/interceptor/logger/LoggerProvider.java
@@ -0,0 +1,46 @@
+package org.example.agent.interceptor.logger;
+
+import org.example.agent.LoggerAgent;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public class LoggerProvider {
+
+  private static final String LOGGER_NAME = LoggerAgent.class.getSimpleName();
+  private static final String LOG_FORMAT = "%s [%s] %s - %s%n";
+  private static final Logger logger = Logger.getLogger(LOGGER_NAME);
+
+  static {
+    setupLogger();
+  }
+
+  public static Logger getLogger() {
+    return logger;
+  }
+
+  private static void setupLogger() {
+    var consoleHandler = new ConsoleHandler();
+    consoleHandler.setFormatter(new CustomFormatter());
+    logger.addHandler(consoleHandler);
+    logger.setUseParentHandlers(false);  // Prevent output to other handlers
+  }
+
+  private static class CustomFormatter extends Formatter {
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT);
+
+    @Override
+    public String format(LogRecord record) {
+      return LOG_FORMAT.formatted(
+          DATE_FORMATTER.format(new Date(record.getMillis())),
+          record.getLoggerName(),
+          record.getLevel().getLocalizedName(),
+          formatMessage(record));
+    }
+  }
+}
